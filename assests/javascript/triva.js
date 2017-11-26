4ee6e3d1d3e19988309181c2bc9115260d3cfb4b
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2017-11-26 00:05:57 -0700

    cleaning up code

---
+++ assests/javascript/triva.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+var ques [
+"What kind of treasure does Barbossa crew steal?", 
+"Why is jack sparrow in port royal?", 
+"Why is the rum gone?", 
+"Who was Will Tuners dad?", 
+"What’s the fastest ship in the Caribbean?",
+"How many pieces of 8 are there?",
+"Which way does Jack Sparrows campus not point?",
+"What is the name of captain Barbossa monkey?",
+];
+var answer1["A.) A fake one",
+			"B.) Lots of it",
+			"C.) A bottle of rum",
+			"D.) A fake one",
+			];
+var answer2['A.) It’s a nice place' 
+			'B.) He is stuck there' 
+			'C.) He is trying to steal a ship' 
+			"D.) To find love" 
+			];
+var answer3['A.) Jack Drank it All ' 
+			'B.) It Got Dropped' 
+			'C.) Elizabeth Started a Signal' 
+			"D.) The Pirates Stole it All" 
+			];
+var answer4['A.) Jack Sparrow' 
+			'B.) Bob Tuner' 
+			'C.) Bootstrap Bill' 
+			"D.) The black smith" 
+			];
+var answer5['A.) The Interceptor' 
+			'B.) The Black Peral' 
+			'C.) The Titanic' 
+			"D.) The Flying Dutchman" 
+			];
+var answer6['A.) 9' 
+			'B.) 3' 
+			'C.) 8' 
+			"D.) 6" 
+			];
+var answer7['A.) Home' 
+			'B.) North' 
+			'C.) West' 
+			"D.) East" 
+			];
+var answer8['A.) Monkey' 
+			'B.) Killer' 
+			'C.) Larry' 
+			"D.) Jack" 
+			];
+	$("#sec").setInterval(myTimer, 1000);
+
+
+
+
+
+}
